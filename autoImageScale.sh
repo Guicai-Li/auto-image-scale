7e5744ddf5d79695c2eab6407ecc678ae03064e6
Guicai.Li <guicai.li.china@gmail.com>
2015-07-06 17:59:25 +0800

    fix 放大失真

--- autoImageScale.sh
+++ autoImageScale.sh
@@ -11,10 +11,29 @@ do
 name1x=1x/${img%@*}.png  
 name2x=2x/${img%@*}@2x.png  
 name3x=3x/${img%@*}@3x.png
+WIDTH=`identify ${img} | cut -f 3 -d " " | sed s/x.*//` #width
+HEIGHT=`identify ${img} | cut -f 3 -d " " | sed s/.*x//` #height
+echo $WIDTH
+echo $HEIGHT
+#计算出1倍图的大小
 echo ${name1x}
-convert -resize 50%x50% ${img} ${name1x}
-echo ${name2x}  
-cp ${img} ${name2x}  
+dw=`echo "${WIDTH}/6" |bc`
+echo $dw
+dh=`echo "${HEIGHT}/6" |bc`
+echo $dh
+convert -resize $dwx$dh ${img} ${name1x}
+#计算出2倍图的大小
+echo ${name2x} 
+dw=`echo "${WIDTH}/3" |bc`
+echo $dw
+dh=`echo "${HEIGHT}/3" |bc`
+echo $dh
+convert -resize $dwx$dh ${img} ${name2x}
+#计算出3倍图的大小
 echo ${name3x}
-convert -resize 150%x150% ${img} ${name3x}
+dw=`echo "${WIDTH}/2" |bc`
+echo $dw
+dh=`echo "${HEIGHT}/2" |bc`
+echo $dh
+convert -resize $dwx$dh ${img} ${name3x}
 done
