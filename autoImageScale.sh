d1c60f8b6fb4ca3274902b10b58bba79ffedfd77
Guicai.Li <guicai.li.china@gmail.com>
2015-07-06 23:10:00 +0800

    remove 多余echo

--- autoImageScale.sh
+++ autoImageScale.sh
@@ -12,28 +12,22 @@ name2x=$path/${img%@*}@2x.png
 name3x=$path/${img%@*}@3x.png
 WIDTH=`identify ${img} | cut -f 3 -d " " | sed s/x.*//` #width
 HEIGHT=`identify ${img} | cut -f 3 -d " " | sed s/.*x//` #height
-echo $WIDTH
-echo $HEIGHT
+
 #计算出1倍图的大小
-echo ${name1x}
 dw=`echo "${WIDTH}/6" |bc`
-echo $dw
 dh=`echo "${HEIGHT}/6" |bc`
-echo $dh
 convert -resize $dwx$dh ${img} ${name1x}
+echo $img @1x is ok 
 #计算出2倍图的大小
-echo ${name2x} 
 dw=`echo "${WIDTH}/3" |bc`
-echo $dw
 dh=`echo "${HEIGHT}/3" |bc`
-echo $dh
 convert -resize $dwx$dh ${img} ${name2x}
+echo $img @2x is ok 
 #计算出3倍图的大小
-echo ${name3x}
 dw=`echo "${WIDTH}/2" |bc`
-echo $dw
 dh=`echo "${HEIGHT}/2" |bc`
-echo $dh
 convert -resize $dwx$dh ${img} ${name3x}
+echo $img @3x is ok 
 rm $img
+echo $img 原图已删除
 done
